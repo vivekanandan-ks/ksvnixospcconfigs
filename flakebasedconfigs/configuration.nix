2769a6af0e8b02fe4070c16c06667a8102d55d1a
vivekanandan-ks <ksvdevksv@gmail.com>
2025-04-14 19:37:05 +0530

    testing

--- flakebasedconfigs/configuration.nix
+++ flakebasedconfigs/configuration.nix
@@ -59,7 +59,7 @@
                             cp configuration.nix flake.lock flake.nix hardware-configuration.nix \
                             "$(date +'%Y-%m-%d_%H-%M')_backup/"'' ;
       ksvsavetogithub = ''eval (ssh-agent -c) && ssh-add ~/.ssh/ghnixospcconfig && git add .  && \
-                          git commit && git-town ship main'' ;
+                          git commit && git push -u origin main'' ;
       ksvnixosproperfullupgrade-boot = ''ksvcdtoflake && ksvnixconfigbackup && \
                                         ksvsavetogithub && nh os boot -ua .'' ;
       ksvnixosproperfullupgrade-switch = ''ksvcdtoflake && ksvnixconfigbackup && \
