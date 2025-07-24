6801d5aed4259b6fdfd868d1e153e66351070669
vivekanandan-ks <ksvdevksv@gmail.com>
2025-07-24 17:42:51 +0530

    trying specialisations

--- configuration.nix
+++ configuration.nix
@@ -217,6 +217,16 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
+  specialisation = {
+    kdeunstable.configuration = {
+      pkgs = pkgs-unstable;
+      # Enable the KDE Plasma Desktop Environment.
+      services.displayManager.sddm.enable = true;
+      services.desktopManager.plasma6.enable = true;
+
+    };
+  };
+
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
