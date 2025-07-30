376daf09960b820f9b3ccb44f29356426c5029cc
vivekanandan-ks <ksvdevksv@gmail.com>
2025-07-30 16:10:00 +0530

    added starship theme files and declared tokyo  theme in starship

--- home.nix
+++ home.nix
@@ -219,27 +219,7 @@ in
       enableNushellIntegration = true;
       enableFishIntegration = true;
       enableBashIntegration = true;
-      settings = {
-
-        add_newline = false;
-
-        format = ''
-          $shell $nix_shell $directory $git_branch $git_status $package $cmd_duration
-          $character
-        '';
-
-        character = {
-          success_symbol = "[>>>](bold fg:green) ";
-          error_symbol = "[>>>](bold fg:red) ";
-        };
-
-        package = {
-          disabled = false;
-        };
-
-        scan_timeout = 10;
-  
-      };
+      settings = builtins.fromTOML (builtins.readFile ./resources/starship-themes/tokyo-night.toml);
     };
 
     #nushell
