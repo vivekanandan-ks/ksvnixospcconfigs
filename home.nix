2e91d3d294c83a9ba2a27027a542828bdff50514
vivekanandan-ks <ksvdevksv@gmail.com>
2025-08-04 02:47:49 +0530

    added pay-respects only as package

--- home.nix
+++ home.nix
@@ -52,7 +52,7 @@ in
       ripgrep-all # same as ripgrep but for many file types like video, PDFs, etc etc
       #nh
       gg-jj
-      #pay-respects
+      pay-respects
 
       /*desktop apps*/
       vlc
@@ -401,7 +401,7 @@ in
   };
 
   #pay-respects
-  programs.pay-respects = {
+  /*programs.pay-respects = {
     enable = true;
     package = pkgs-unstable.pay-respects;
     #enableBashIntegration = true;
@@ -413,7 +413,7 @@ in
     #added a home.file below
   };
   #pay-respects config file
-  /*home.file.".config/pay-respects/config.toml" = {
+  home.file.".config/pay-respects/config.toml" = {
       text = ''
         package_manager.install_method = "Shell"
       '';
