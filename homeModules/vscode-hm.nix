d9df89cf021096e844469be898a38d4458e57cd9
vivekanandan-ks <ksvdevksv@gmail.com>
2025-08-29 18:55:11 +0530

    -(hardcoded wakatime api)

--- homeModules/vscode-hm.nix
+++ homeModules/vscode-hm.nix
@@ -110,9 +110,6 @@ in
           "editor.defaultFormatter" = "jnoortheen.nix-ide";
         };
         "update.mode" = "none";
-        #"wakatime.apiKey" = builtins.readFile ./hmResources/.hmSecrets/wakatimeapikey.txt;
-        "wakatime.apiKey" = builtins.readFile ../../wakatimeapikey.txt;
-
       };
 
     };
