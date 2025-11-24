bc00c411650c547e45b682c67c1d20ae9ac1638d
vivekanandan-ks <ksvdevksv@gmail.com>
2025-11-24 12:43:06 +0530

    Default shell changed to nushell from fish shell

--- configuration.nix
+++ configuration.nix
@@ -432,7 +432,7 @@
       "wheel"
     ];
     hashedPassword = "$6$DmrUUL7YWFMar6aA$sAoRlSbFH/GYETfXGTGa6GSTEsBEP1lQ6oRdXlQUsqhRB7OTI2vTmVlx64B2ihcez8B0q0l8/Vx1pO8c82bxm0";
-    shell = pkgs-unstable.fish;
+    shell = pkgs-unstable.nushell;
     packages =
       (with pkgs; [
         #stable
