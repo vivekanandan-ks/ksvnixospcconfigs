2f7fc63e2ad09924388d3409e5d5c8dcdb72f325
vivekanandan-ks <ksvdevksv@gmail.com>
2026-01-06 23:53:47 +0530

    correct zellij options under settings

--- homeModules/zellij-hm.nix
+++ homeModules/zellij-hm.nix
@@ -13,16 +13,18 @@
     package = pkgs-unstable.zellij;
     enableBashIntegration = true;
     enableFishIntegration = true;
-    show_startup_tips = false;
-    ui = {
-      pane_frames = {
-        rounded_corners = true;
-        #hide_session_name = false;
-      };
-    };
+    
     settings = {
       #theme = "custom";
       #themes.custom.fg = "#ffffff";
+
+      show_startup_tips = false;
+      ui = {
+        pane_frames = {
+          rounded_corners = true;
+          #hide_session_name = false;
+        };
+      };
       
     };
   };
