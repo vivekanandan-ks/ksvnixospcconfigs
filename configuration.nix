7f5b59e78e0c3a00613f7490b4cd73c1eb5a0a48
vivekanandan-ks <ksvdevksv@gmail.com>
2026-02-19 14:30:05 +0530

    disabled default networking since I use iwd

--- configuration.nix
+++ configuration.nix
@@ -229,7 +229,7 @@
   */
 
   #networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
