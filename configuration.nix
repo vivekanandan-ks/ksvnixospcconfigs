2b2eb9686d9b4d81142e1dce9ef25c311191b83e
vivekanandan-ks <ksvdevksv@gmail.com>
2026-02-19 15:40:52 +0530

    iwd option working, networking defaults changed to iwd

--- configuration.nix
+++ configuration.nix
@@ -229,7 +229,7 @@
   */
 
   #networking.hostName = "nixos"; # Define your hostname.
-  networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -243,6 +243,7 @@
     enable = true;
     package = pkgs-unstable.iwd;
   };
+  networking.networkmanager.wifi.backend = "iwd";
 
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
