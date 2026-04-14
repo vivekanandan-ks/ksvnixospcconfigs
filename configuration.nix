6ee6bc62dcd8dbd1d944b050ee21e8fce1d6caaa
vivekanandan-ks <ksvdevksv@gmail.com>
2026-04-15 03:51:21 +0530

    default shell changed to fish from nu

--- configuration.nix
+++ configuration.nix
@@ -320,6 +320,8 @@
   users.mutableUsers = false; # this will make all user management only via nixos and
   #imperative user creations or anything in kde or commands won't persist the nixos-rebuild command.
 
+  programs.fish.enable = true;
+
   #root password
   users.users.root.hashedPassword = "$6$/Yo/IR.A6rGbFVr6$a6c7yhjPYGuJOBBkcPXl/SjZ531tEUHtkY3tX3np2dcX6JpZg.Myrwdnz.fhqci0Sg83vU8lDYmdpSAQqD.OF0";
   # Define a user account
@@ -331,7 +333,8 @@
       "wheel"
     ];
     hashedPassword = "$6$DmrUUL7YWFMar6aA$sAoRlSbFH/GYETfXGTGa6GSTEsBEP1lQ6oRdXlQUsqhRB7OTI2vTmVlx64B2ihcez8B0q0l8/Vx1pO8c82bxm0";
-    shell = pkgs-unstable.nushell;
+    #shell = pkgs-unstable.nushell;
+    shell = pkgs-unstable.fish;
     packages =
       (with pkgs; [
         #stable
