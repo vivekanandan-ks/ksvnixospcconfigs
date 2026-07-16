f451e688727df2b111f2d167dc2f7fd768162ad0
vivekanandan-ks <ksvdevksv@gmail.com>
2026-07-16 13:15:18 +0530

    hyprland and it's plugin added

--- flake.nix
+++ flake.nix
@@ -85,7 +85,24 @@
     };
 
     # hyprland
-    #hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "github:hyprwm/Hyprland";
+
+    # hyprland official plugins
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+
+    hypr-dynamic-cursors = {
+      url = "github:VirtCode/hypr-dynamic-cursors";
+      inputs.hyprland.follows = "hyprland"; # to make sure that the plugin is built for the correct version of hyprland
+    };
+
+    gloview = {
+      url = "github:fedsfarm/gloview";
+      inputs.hyprland.follows = "hyprland";
+    };
+
     /*
     hyprland = {
       #type = "git";
@@ -94,11 +111,7 @@
       #inputs.nixpkgs.follows = "nixpkgs"; # commenting means we use latest hyprland directly
     };
 
-    # hyprland official plugins
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-    };
+
 
     # a hyprland plugin refer: https://github.com/KZDKM/Hyprspace
     Hyprspace = {
@@ -111,11 +124,6 @@
     hyprtasking = {
       url = "github:raybbian/hyprtasking";
       inputs.hyprland.follows = "hyprland";
-    };
-
-    hypr-dynamic-cursors = {
-      url = "github:VirtCode/hypr-dynamic-cursors";
-      inputs.hyprland.follows = "hyprland"; # to make sure that the plugin is built for the correct version of hyprland
     };
     */
 
---
+++ flakepartsModules/hyprland-fp.nix
@@ -0,0 +1,39 @@
+{...}: {
+  flake = {
+    nixosModules.hyprland = {
+      inputs,
+      pkgs,
+      #pkgs-unstable,
+      ...
+    }: {
+      programs.hyprland = {
+        enable = true;
+        xwayland.enable = true; # default true
+        package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+        portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      };
+    };
+
+    homeModules.nonDroid.hyprland = {
+      inputs,
+      pkgs,
+      #pkgs-unstable,
+      ...
+    }: {
+      wayland.windowManager.hyprland = {
+        enable = true;
+        package = null;
+        portalPackage = null;
+        configType = "lua";
+        plugins = [
+          inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprbars
+          inputs.hypr-dynamic-cursors.packages.${pkgs.stdenv.hostPlatform.system}.hypr-dynamic-cursors
+          inputs.gloview.packages.${pkgs.system}.gloview
+        ];
+        systemd.variables = ["--all"];
+
+        settings.bind = ["SUPER, TAB, gloview:toggle"];
+      };
+    };
+  };
+}
