1c798b30bc7d6b05b5336cfda295f3f2b34d1cdd
vivekanandan-ks <ksvdevksv@gmail.com>
2026-07-16 09:13:14 +0530

    some noctalia changes

--- flakepartsModules/noctalia/noctalia-fp.nix
+++ flakepartsModules/noctalia/noctalia-fp.nix
@@ -6,12 +6,16 @@
   perSystem = {pkgs, ...}: {
     packages.ksvNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
       inherit pkgs;
-      settings =
+      settings = pkgs.lib.recursiveUpdate
         (
           builtins.fromJSON (
             builtins.readFile ./ksv-noctalia.json
           )
-        ).settings;
+        ).settings
+        {
+          general.avatarImage = "${./shoyohinata.png}";
+          wallpaper.directory = "${../nixosfpModules/nixosResources/limine-images}";
+        };
 
       preInstalledPlugins = {
         clipper = {
@@ -53,6 +57,14 @@
         workspace-overview = {
           enabled = true;
           src = "${inputs.noctalia-legacy-v4-plugins.outPath}/workspace-overview";
+        };
+        mpvpaper = {
+          enabled = false;
+          src = "${inputs.noctalia-legacy-v4-plugins.outPath}/mpvpaper";
+        };
+        video-wallpaper = {
+          enabled = false;
+          src = "${inputs.noctalia-legacy-v4-plugins.outPath}/video-wallpaper";
         };
       };
     };
