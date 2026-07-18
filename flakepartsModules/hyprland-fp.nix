eb737cbd8e3c7cc73f4c931f05c9111b89e72ec4
vivekanandan-ks <ksvdevksv@gmail.com>
2026-07-18 13:06:23 +0530

    some hyprland configurations, WIP

--- flakepartsModules/hyprland-fp.nix
+++ flakepartsModules/hyprland-fp.nix
@@ -37,9 +37,73 @@
           inputs.hypr-dynamic-cursors.packages.${pkgs.stdenv.hostPlatform.system}.hypr-dynamic-cursors
           #inputs.gloview.packages.${pkgs.system}.gloview
         ];
-        systemd.variables = ["--all"];
+        systemd = {
+          enable = true;
+          variables = ["--all"];
+        };
 
         settings = {
+
+          misc = {
+            # Window swallowing
+            # (i.e. children window causes parent to be hidden)
+            enable_swallow = true; # Enable window swallowing
+            swallow_regex = "kitty|ghostty"; # Windows for which swallowing is applied
+
+            # dpms
+            mouse_move_enables_dpms = true; # Enable DPMS on mouse/touchpad action
+            key_press_enables_dpms = true; # Enable DPMS on keyboard action
+            disable_autoreload = true; # Autoreload is unnecessary on NixOS, because the configuration file is read-only link
+          };
+
+          general = {
+            # gaps
+            gaps_in = 4;
+            gaps_out = 8;
+          };
+
+          input = {
+            follow_mouse = 1;
+          };
+
+          animations = {
+            enabled = true;
+            first_launch_animation = true; # fade in on first launch
+            bezier = [
+              "smoothOut, 0.36, 0, 0.66, -0.56"
+              "smoothIn, 0.25, 1, 0.5, 1"
+              "overshot, 0.4,0.8,0.2,1.2"
+            ];
+
+            animation = [
+              "windows, 1, 4, overshot, slide"
+              "windowsOut, 1, 4, smoothOut, slide"
+              "border,1,10,default"
+
+              "fade, 1, 10, smoothIn"
+              "fadeDim, 1, 10, smoothIn"
+              "workspaces,1,4,overshot,slidevert"
+            ];
+
+          };
+
+          decoration = {
+            # fancy corners
+            rounding = 7;
+
+            blur = {
+              enabled = true;
+              size = 5;
+              passes = 3;
+              ignore_opacity = true;
+              new_optimizations = 1;
+              xray = true;
+              #contrast = 0.7;
+              #brightness = 0.8;
+              #vibrancy = 0.2;
+              #special = true; # expensive, but helps distinguish special workspaces
+            };
+          };
           exec-once = [
             "${lib.getExe self.packages.${pkgs.stdenv.hostPlatform.system}.ksvNoctalia}"
             "systemctl --user start hyprpolkitagent"
