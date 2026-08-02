513b698a8e6de28139b17016aea2f9224a9c88e5
vivekanandan-ks <ksvdevksv@gmail.com>
2026-08-02 20:46:36 +0530

    determinate substituters added and zen transparency in hyprland changed to 0.9

--- flakepartsModules/hyprland/zen-transparency-fp.nix
+++ flakepartsModules/hyprland/zen-transparency-fp.nix
@@ -3,7 +3,7 @@
     homeModules.nonDroid.zen-transparency = {
       wayland.windowManager.hyprland.settings = {
         windowrule = [
-          "opacity 0.99 0.99, match:class ^(zen.*)$"
+          "opacity 0.9 0.9, match:class ^(zen.*)$"
         ];
       };
     };
--- flakepartsModules/nixosfpModules/determinate-fp.nix
+++ flakepartsModules/nixosfpModules/determinate-fp.nix
@@ -7,5 +7,15 @@
     imports = [
       inputs.determinate.nixosModules.default # sets detsys nix as the default nix
     ];
+
+    nix.settings = {
+      extra-substituters = [
+        "https://install.determinate.systems"
+      ];
+
+      extra-trusted-public-keys = [
+        "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
+      ];
+    };
   };
 }
