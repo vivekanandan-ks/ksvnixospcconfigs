bd42c3a57c890cdff928f416431a48f058426443
vivekanandan-ks <ksvdevksv@gmail.com>
2026-08-07 01:57:22 +0530

    dolphin apps selection not showing fixed

---
+++ flakepartsModules/hyprland/hypr-dolph-fix-fp.nix
@@ -0,0 +1,24 @@
+{...}: {
+  flake = {
+    nixosModules.hypr-dolph-fix = { pkgs, ... }: {
+      xdg.menus.enable = true;
+      xdg.mime.enable = true;
+      environment.etc."xdg/menus/applications.menu" = {
+        source = "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+      };
+    };
+
+    homeModules.nonDroid.hypr-dolph-fix = { lib, pkgs, ... }: {
+      home.activation.rebuildKDECache = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        rm -rf ~/.cache/ksycoca*
+        ${pkgs.kdePackages.kservice}/bin/kbuildsycoca6 --noincremental
+      '';
+
+      wayland.windowManager.hyprland.settings = {
+        env = [
+          "XDG_MENU_PREFIX,plasma-"
+        ];
+      };
+    };
+  };
+}
