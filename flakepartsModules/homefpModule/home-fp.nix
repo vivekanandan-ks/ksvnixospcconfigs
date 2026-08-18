4d3cd7521ff9e993724eb8b79c7942d32dce37f6
vivekanandan-ks <ksvdevksv@gmail.com>
2026-08-19 02:25:25 +0530

    refactor: use lib.mkDefault for system and home stateVersion and deduplicate declarations

--- flakepartsModules/homefpModule/home-fp.nix
+++ flakepartsModules/homefpModule/home-fp.nix
@@ -129,7 +129,7 @@
     # You should not change this value, even if you update Home Manager. If you do
     # want to update the value, then make sure to first check the Home Manager
     # release notes.
-    home.stateVersion = "24.11"; # Please read the comment before changing.
+    #home.stateVersion = "24.11"; # Please read the comment before changing. (Set via lib.mkDefault in common.base)
 
     # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
--- flakepartsModules/nixosfpModules/configuration-nixos-fp.nix
+++ flakepartsModules/nixosfpModules/configuration-nixos-fp.nix
@@ -171,6 +171,6 @@ _: {
     # this value at the release version of the first install of this system.
     # Before changing this value read the documentation for this option
     # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "24.11"; # Did you read the comment?
+    system.stateVersion = lib.mkDefault "24.11"; # Did you read the comment?
   };
 }
