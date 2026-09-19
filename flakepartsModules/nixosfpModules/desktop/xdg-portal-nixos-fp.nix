04f8624a870eb7ef9d74e3c9e53b117a84b9cca2
vivekanandan-ks <ksvdevksv@gmail.com>
2026-09-19 14:34:09 +0530

    fix(portal): restore wlr screencast and comment out luminous

--- flakepartsModules/nixosfpModules/desktop/xdg-portal-nixos-fp.nix
+++ flakepartsModules/nixosfpModules/desktop/xdg-portal-nixos-fp.nix
@@ -9,40 +9,47 @@
       xdg.portal = {
         enable = true;
 
-        # Keep WLR enabled as fallback
+        # Keep WLR enabled as primary/fallback portal for screencast & screenshot
         wlr = {
           enable = true;
           settings = {
             screencast = {
               chooser_type = "dmenu";
               chooser_cmd = "${pkgs-unstable.wofi}/bin/wofi --dmenu --prompt 'Select Screen to Share'";
+              # Alternative with rofi if preferred:
+              # chooser_cmd = "${pkgs-unstable.rofi}/bin/rofi -dmenu -i -p 'Select Screen to Share'";
             };
           };
         };
 
         extraPortals = [
-          pkgs-unstable.xdg-desktop-portal-luminous
+          # pkgs-unstable.xdg-desktop-portal-luminous
           pkgs-unstable.xdg-desktop-portal-gtk
         ];
 
         config = {
           common.default = ["gtk"];
 
+          # Prioritize WLR for Mango, fallback to GTK
+          mango.default = lib.mkForce ["wlr" "gtk"];
+
           # Prioritize Luminous for Mango, fallback to WLR, then GTK
-          mango = {
-            default = lib.mkForce ["luminous" "wlr" "gtk"];
-            "org.freedesktop.impl.portal.ScreenCast" = lib.mkForce ["luminous" "wlr"];
-            "org.freedesktop.impl.portal.Screenshot" = lib.mkForce ["luminous" "wlr"];
-          };
+          # mango = {
+          #   default = lib.mkForce ["luminous" "wlr" "gtk"];
+          #   "org.freedesktop.impl.portal.ScreenCast" = lib.mkForce ["luminous" "wlr"];
+          #   "org.freedesktop.impl.portal.Screenshot" = lib.mkForce ["luminous" "wlr"];
+          # };
+          niri.default = ["gnome" "gtk"];
+          sway.default = ["wlr" "gtk"];
         };
       };
     };
 
-    homeModules.nonDroid.xdg-portal = { ... }: {
-      xdg.configFile."xdg-desktop-portal-luminous/config.toml".text = ''
-        color_scheme = "${self.personas.ksv.theme_polarity}"
-        screenshot_permission_check = false
-      '';
-    };
+    # homeModules.nonDroid.xdg-portal = { ... }: {
+    #   xdg.configFile."xdg-desktop-portal-luminous/config.toml".text = ''
+    #     color_scheme = "${self.personas.ksv.theme_polarity}"
+    #     screenshot_permission_check = false
+    #   '';
+    # };
   };
 }
